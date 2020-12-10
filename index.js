97c921922df7004c00bdd642158e04dca29025c8
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-10 10:34:21 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+
+let  multiply = num1 * num2; 
+let random = (Math.random() * 100);
+let mod = 24 % 20 ;
+let max = Math.max(1,2,3,4,20);
